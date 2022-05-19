7aac0467b16c72f80a7f8c5e37880d0c28f5ce71
n-mariachristina <91893344+n-mariachristina@users.noreply.github.com>
2022-05-19 14:05:44 -0400

    Initial files

---
+++ Button.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Button here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Button extends Actor
+{
+    /**
+     * Act - do whatever the Button wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }  
+    
+    public Button() {
+        GreenfootImage buttonImg = new GreenfootImage("button.png");
+        buttonImg.scale(buttonImg.getWidth()/4,buttonImg.getHeight()/4);
+        setImage(buttonImg);
+    }
+}
